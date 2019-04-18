f4333c96aa7ff3730618eab0f25cf97c647494a6
superPershing <727342027@qq.com>
2019-04-18 19:50:04 +0800

    test(AnswerGenerator): add test for AnswerGenerator

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,19 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
-
+    @Test
+    public void test_answer_generator() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        assertEquals(answerGenerator.generate().toString().split(" ").length, 4);
+    }
 }
 
